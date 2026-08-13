9248241802a5d7719d4ea8ae8e6eaf904716790c
MeghanW23 <wmeghan0809@gmail.com>
2026-08-13 14:10:11 -0400

    edit sbatch formatting in docker-to-sif_batch-script.sh

--- scripts/MotionCharacterization/docker-to-sif_batch-script.sh
+++ scripts/MotionCharacterization/docker-to-sif_batch-script.sh
@@ -2,14 +2,15 @@
 
 # Please see official Slurm documentation for more on SBATCH arguments - https://slurm.schedmd.com/sbatch.html
 
-#SBATCH --partition=bch-compute                       # queue to be used - bch-compute is the default
-#SBATCH --time=02:05:00                           # Running time (in hours-minutes-seconds) - only request what you need
-#SBATCH --job-name=sif-conversion               # Job name - you should change this
-#SBATCH --mail-type=BEGIN,END,FAIL            # send and email when the job begins, ends or fails
-#SBATCH --mail-user=meghan.walsh2@childrens.harvard.edu    # Email address to send the job status - can use api endpoints, see wiki
-#SBATCH --output=output_%j.txt                      # Name of the output file - this format is jobid
-#SBATCH --cpus-per-task=4                   # Number of CPUs to request - default is 1 - change for multithreading
-#SBATCH --mem=31000M                          # RAM to request - you should change this to meet your needs
+
+#SBATCH --partition=bch-compute
+#SBATCH --time=02:05:00
+#SBATCH --job-name=sif-conversion
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=meghan.walsh2@childrens.harvard.edu
+#SBATCH --output=output_%j.txt
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=31000M
 
 
 # ------------ Batch Processing Script ------------
@@ -31,7 +32,7 @@ whoami
 echo "This is SLURM_JOB_ID: "
 echo $SLURM_JOB_ID
 
-rm ${TMPDIR}/slimm.sif
+# rm ${TMPDIR}/sms-mi-reg.sif
 
 echo `date`
 APPTAINER_CACHEDIR=${TMPDIR} APPTAINER_TMPDIR=${TMPDIR} apptainer build ${TMPDIR}/sms-mi-reg.sif docker-archive:${TMPDIR}/sms-mi-reg.tar
