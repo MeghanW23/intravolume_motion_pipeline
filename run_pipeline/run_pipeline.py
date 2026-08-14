93c0403ff623068c7f36ab629575e5038c4b3bdf
MeghanW23 <wmeghan0809@gmail.com>
2026-08-14 14:32:01 -0400

    move corrected image search to its own function

--- run_pipeline/run_pipeline.py
+++ run_pipeline/run_pipeline.py
@@ -149,44 +149,15 @@ class RunPipeline:
             )
         """
         ========================================
-        STEP THREE: FMRIPREP CORRECTION
+        STEP THREE: FMRIPREP
         ========================================
         """
         if configurations.RUN_FMRIPREP:
-
-            scrubbed_image: str = ""
-            matching_scrubbed_files: list[str] = glob(os.path.join(configurations.OUTPUT_DIRECTORY_PATH, configurations.MCORR_OUTPUT_FILENAME_PATTERN))
-            if len(matching_scrubbed_files) == 0:
-                scrubbed_image: str = os.path.join(configurations.OUTPUT_DIRECTORY_PATH, configurations.MCORR_ABRUPTMOTION_FILE_NAME) 
-                print()
-                print()
-                warnings.warn(
-                    message=(
-                        f"Could not find scrubbed data matching pattern: {configurations.MCORR_OUTPUT_FILENAME_PATTERN}"
-                        f"Using file: {scrubbed_image}. "
-                        "If your data has no above-threshold motion, this is expected. "
-                        "If not, something has gone wrong. "
-                    ),
-                    category=UserWarning
-                    
-                )
-
-            elif len(matching_scrubbed_files) > 1:
-                scrubbed_image: str = sorted(matching_scrubbed_files, key=os.path.getmtime)[-1]
-                warnings.warn(
-                    message=(
-                        f"More than one srubbed NiFTI Image Found: {matching_scrubbed_files}. "
-                        f"Using the most recently modified image: {scrubbed_image}"
-                    ),
-                    category=UserWarning
-                )
-            else:
-                scrubbed_image: str = matching_scrubbed_files[0]
-
+            corrected_image: str = self.find_intravolume_corrected_data(configurations)
 
             StartSingleRunfMRIPrep(
                 func_data=[
-                    scrubbed_image, 
+                    corrected_image, 
                     func_json_file_path],
                 anat_data=\
                     [configurations.ANATOMICAL_DICOM_DIRECTORY] if configurations.ANATOMICAL_DICOM_DIRECTORY
@@ -205,7 +176,37 @@ class RunPipeline:
                 omp_nthreads=configurations.OMP_NTHREADS, # pyright: ignore[reportArgumentType],
                 mem_mb=configurations.MEM_MB # pyright: ignore[reportArgumentType]
             )
-             
+
+    def find_intravolume_corrected_data(self, configurations: Configurations) -> str:
+        corrected_image: str = ""
+        matching_corrected_files: list[str] = glob(os.path.join(configurations.OUTPUT_DIRECTORY_PATH, configurations.MCORR_OUTPUT_FILENAME_PATTERN))
+        if len(matching_corrected_files) == 0:
+            corrected_image: str = os.path.join(configurations.OUTPUT_DIRECTORY_PATH, configurations.MCORR_ABRUPTMOTION_FILE_NAME) 
+            warnings.warn(
+                message=(
+                    f"Could not find scrubbed data matching pattern: {configurations.MCORR_OUTPUT_FILENAME_PATTERN}"
+                    f"Using file: {corrected_image}. "
+                    "If your data has no above-threshold motion, this is expected. "
+                    "If not, something has gone wrong. "
+                ),
+                category=UserWarning
+            )
+            return corrected_image
+
+        elif len(matching_corrected_files) > 1:
+            corrected_image: str = sorted(matching_corrected_files, key=os.path.getmtime)[-1]
+            warnings.warn(
+                message=(
+                    f"More than one scrubbed NiFTI Image Found: {matching_corrected_files}. "
+                    f"Using the most recently modified image: {corrected_image}"
+                ),
+                category=UserWarning
+            )
+            return corrected_image
+        
+        else:
+            return matching_corrected_files[0]        
+
 
 if __name__ == "__main__":
     import argparse
